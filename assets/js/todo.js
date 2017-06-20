321673ffdd952e4c72696d1bbf66f04432199105
Borhan <net2net4all@gmail.com>
2017-06-20 14:00:57 -0700

    style added

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,6 @@
+
+
+
 // Check off a specific item
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
@@ -11,17 +14,24 @@ $("ul").on("click", "span", function(event){
 	event.stopPropagation();
 });
 
-
+// Add a new task under all the exsiting tasks
 $("input[type='text'").keypress(function(event){
 	if(event.which === 13){
 		//this is the text user entered
 		var userText = $(this).val();
 
 		//create a new li inside ul
-		$("ul").append("<li><span>x </span>"+userText+"</li>");
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='ture'></i></span><i class='fa fa-arrow-right' aria-hidden='true'></i>"+userText+"</li>");
 
 		//clear the input
 		$("input[type='text'").val("");
 
 	}
 });
+
+
+// show or hide the "Add New To-Do Task" field
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+
